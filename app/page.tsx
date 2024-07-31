e37f0bd8eeb5fbcd443741e71131c4e91052bc66
joaogabriellyra <jgabriellyra@hotmail.com>
2024-07-31 15:21:49 -0300

    created a component for the header content

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-import Image from 'next/image'
-import { Timer, Scroll } from '@phosphor-icons/react/dist/ssr'
-import logo from '@/public/logo.svg'
+import Header from './header'
 
 export default function Page() {
   return (
     <>
-      <header>
-        <Image
-          src={logo}
-          width={logo.width}
-          height={logo.height}
-          alt="rocketseat logo"
-        />
-        <div>
-          <Timer width={27} height={32} />
-          <Scroll width={35} height={32} />
-        </div>
-      </header>
+      <Header />
     </>
   )
 }
